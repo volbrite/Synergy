7c32fd22cb9bf8743c6e1ff3b1598859b39d1ab4
Michal Bizon <volbrite@gmail.com>
2015-03-04 17:19:31 +0100

    siema

---
+++ Sources/main.c
@@ -0,0 +1,28 @@
+/*
+ * main implementation: use this 'C' sample to create your own application
+ *
+ */
+
+
+
+
+
+#include "derivative.h" /* include peripheral declarations */
+
+
+
+int main(void)
+{
+	int counter = 0;
+	int dupa = 3;
+	int jajebie123 = 3; 
+	///CHUJ WAM W DUPE/////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////
+	//////////////////////////:) :) :)
+	
+	for(;;) {	   
+	   	counter++;
+	}
+	
+	return 0;
+}
